3ef3003a64c24dc53798b080d6588aadbf0a3a29
annkit-singh <ankit871681@gmail.com>
2022-09-24 22:12:20 +0530

    his changes

--- src/main/java/com/example/demo/TestingClass.java
+++ src/main/java/com/example/demo/TestingClass.java
@@ -6,6 +6,8 @@ public class TestingClass {
     public static void main(String[] args) {
         System.out.println("System is active");
         new TestingClass().sayHello();
+        aankit;
+        Jonar;
 
 
     }
