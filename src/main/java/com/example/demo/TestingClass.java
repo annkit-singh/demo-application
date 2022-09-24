a54cc08d65d03972d9b1ad89556fa1acdd6db76b
annkit-singh <ankit871681@gmail.com>
2022-09-24 22:05:03 +0530

    project added

---
+++ src/main/java/com/example/demo/TestingClass.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+import java.util.stream.Stream;
+
+public class TestingClass {
+    public static void main(String[] args) {
+        System.out.println("System is active");
+        new TestingClass().sayHello();
+
+
+    }
+
+    public void sayHello(){
+        System.out.println("Hello Program");
+    }
+}
+
+
+
+
+
